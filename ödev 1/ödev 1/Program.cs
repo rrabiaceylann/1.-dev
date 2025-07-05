2e7b9663dfed7b138139dd0d8d9b6237e57f3284
Rabia <rabiya_87@hotmail.com>
2025-07-06 00:52:11 +0300

    Add files via upload

---
+++ ödev 1/ödev 1/Program.cs
@@ -0,0 +1,3 @@
+﻿System.Console.WriteLine("Rabia Ceylan"); 
+
+System.Console.WriteLine("Patika'nın uzman kadrosu ve tecrübeli egitmenleri sayesinde yazilim dünyasina hizi bir sekilde giris apabilecegim için bu bootcampteyim!");
